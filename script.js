c322c05b077409ed7afb3f6159329d8dc76abc4a
Pavithravnb <pavisomi2000@gmail.com>
2024-10-02 17:57:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Script for the "Click Me!" button
+document.getElementById("clickMeButton").addEventListener("click", function() {
+    document.getElementById("message").innerText = "Hello, welcome to my static website!";
+});
